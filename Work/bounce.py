5081cdc4a8a3433405b7e4797497f0b129e81fe6
fabsocks <fabiocalsamiglia@gmail.com>
2020-06-09 16:19:04 +0200

    aggiornamento

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+# A rubber ball is dropped from a height of 100 meters and each time it hits the ground,
+# it bounces back up to 3/5 the height it fell.
+# Write a program bounce.py that prints a table showing the height of the first 10 bounces
+bounces = 0
+bounce_ratio = 3/5
+height = 100
+
+while bounces < 10:
+    height = round(height * bounce_ratio, 4)
+    bounces = bounces + 1
+    print("Bounce:", bounces, " Height:", height)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,24 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    """Carica il contenuto del file in una lista di tuple"""
+
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        header = next(f)
+        for row in rows:
+            holding = (row[0], row[1], row[2])
+            portfolio.append(holding)
+    return portfolio
+
+
+
+
+
+
+
+print(read_portfolio("Data/portfolio.csv"))
