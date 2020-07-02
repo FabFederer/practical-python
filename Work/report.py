cb67bf4586b5c001dd19971bd8a2b541958a2074
fabsocks <fabiocalsamiglia@gmail.com>
2020-07-02 17:04:41 +0200

    Aggiornamento

--- Work/report.py
+++ Work/report.py
@@ -1,24 +1,22 @@
-# report.py
-#
-# Exercise 2.4
-import csv
-
-def read_portfolio(filename):
-    """Carica il contenuto del file in una lista di tuple"""
-
-    portfolio = []
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        header = next(f)
-        for row in rows:
-            holding = (row[0], row[1], row[2])
-            portfolio.append(holding)
-    return portfolio
+from collections import Counter
+from collections import defaultdict
 
 
+portfolio = [
+    ('GOOG', 100, 490.1),
+    ('IBM', 50, 91.1),
+    ('CAT', 150, 83.44),
+    ('IBM', 100, 45.23),
+    ('GOOG', 75, 572.45),
+    ('AA', 50, 23.15)
+]
+
+total_shares = Counter()
+holdings = defaultdict(list)
+for name, shares, price in portfolio:
+    total_shares[name] += shares
+    holdings[name].append((shares, price))
 
 
-
-
-
-print(read_portfolio("Data/portfolio.csv"))
+print(total_shares['IBM'])
+print(holdings['GOOG'])
